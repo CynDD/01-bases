cea0d24d6f7da9af9c3f28be7f0d8b5afc2ea5fb
CynDD <cynthia.duhalde@gmail.com>
2021-09-02 10:37:47 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { HeroeModule } from './heroes/listado/heroes.module';
+import { ContadorModule } from './contador/contador.module';
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+ ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroeModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
